636046393467cad68ac01122a4eed539c1427464
richardmarquet <richardmarquet@Richards-MacBook-Pro.local>
2024-07-01 21:20:22 -0700

    quick chanegs

--- app/clientportal/(general)/users/_components/DataTable.tsx
+++ app/clientportal/(general)/users/_components/DataTable.tsx
@@ -68,10 +68,10 @@ export function DataTable<TData, TValue>({
     <div>
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          placeholder="Filter usernames..."
+          value={(table.getColumn("username")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
+            table.getColumn("username")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
